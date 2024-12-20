f9eca17284d37574b795123a780b86e1bf6cfd46
gil-fullstack <gilservicosweb@gmail.com>
2024-12-20 16:53:51 -0300

    QuestionDisplay

--- app/src/main/java/com/tinellus/jettrivia/component/Questions.kt
+++ app/src/main/java/com/tinellus/jettrivia/component/Questions.kt
@@ -1,53 +1,186 @@
 package com.tinellus.jettrivia.component
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.tinellus.jettrivia.model.QuestionItem
 import com.tinellus.jettrivia.screens.QuestionsViewModel
 import com.tinellus.jettrivia.util.AppColors
 
 @Composable
 fun Questions(viewModel: QuestionsViewModel){
     val questions = viewModel.data.value.data?.toMutableList()
+    val questionIndex = remember {
+        mutableStateOf(0)
+    }
     if (viewModel.data.value.loading == true){
         CircularProgressIndicator()
         Log.d("LOADING", "Questions ---Loading: ${questions?.size}")
     }
-//    else{
-//        Log.d("SIZE", "Questions: Loading STOPPED")
-//        questions?.forEach {questionItem ->
-//            Log.d("Result", "Questions: ${questionItem.question}")
-//        }
-//    }
+    else{
+        val question = try {
+            questions?.get(questionIndex.value)
+        }catch (ex: Exception){
+            null
+        }
+
+        if (questions != null) {
+            QuestionDisplay(question = question!!, questionIndex = questionIndex,
+                viewModel = viewModel){
+                questionIndex.value = questionIndex.value + 1
+
+            }
+        }
+    }
 }
-@Preview(showBackground = true)
+
 @Composable
-fun QuestionDisplay() {
-    Surface(modifier = Modifier.fillMaxSize()
-        .padding(4.dp),
-        color = AppColors.mLightGray
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+    val answerState = remember(question) {
+        mutableStateOf<Int?>(null)
+
+    }
+    val correctAnswerState = remember(question) {
+        mutableStateOf<Boolean?>(null)
+
+    }
+    val updateAnswer: (Int) -> Unit = remember(question) {
+        {
+            answerState.value = it
+            correctAnswerState.value = choicesState[it] == question.answer
+        }
+
+    }
+
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+    Surface(modifier = Modifier.fillMaxWidth()
+        .fillMaxHeight(),
+        color = AppColors.mDarkPurple
     ) {
         Column(modifier = Modifier.padding(12.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start) {
+
+            if (questionIndex.value >= 3) ShowProgress(score = questionIndex.value)
             QuestionTracker()
+            DrawDottedLine(pathEffect)
+            Column {
+                Text(text = question.question,
+                    modifier = Modifier.padding(6.dp),
+                    fontSize = 17.sp,
+                    color = AppColors.mOffWhite,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp)
+
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(modifier = Modifier
+                        .padding(3.dp)
+                        .fillMaxWidth()
+                        .height(55.dp)
+                        .border(width = 4.dp,
+                            brush = Brush.linearGradient(colors = listOf(AppColors.mOffDarkPurple,
+                                AppColors.mOffDarkPurple)),
+                            shape = RoundedCornerShape(15.dp))
+                        .clip(RoundedCornerShape(topStartPercent = 50,
+                            topEndPercent = 50,
+                            bottomEndPercent = 50,
+                            bottomStartPercent = 50))
+                        .background(Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically) {
+                        RadioButton(selected = (answerState.value == index),
+                            onClick = {
+                                updateAnswer(index)
+                            },
+                            modifier = Modifier.padding(start = 16.dp),
+                            colors = RadioButtonDefaults
+                                .colors(selectedColor = if (correctAnswerState.value == true && index == answerState.value) {
+                                    Color.Green.copy(alpha = 0.2f)
+                                } else {
+                                    Color.Red.copy(alpha = 0.2f)
+                                })) //end rb
+
+                        val annotatedString = buildAnnotatedString {
+                            withStyle(style = SpanStyle(fontWeight = FontWeight.Light,
+                                color = if (correctAnswerState.value == true && index == answerState.value) {
+                                    Color.Green
+                                } else if (correctAnswerState.value == false && index == answerState.value) {
+                                    Color.Red
+                                } else {
+                                    AppColors.mOffWhite
+                                },
+                                fontSize = 17.sp)){
+
+                                append(answerText)
+                            }
+                        }
+                        Text(text = annotatedString, modifier = Modifier.padding(6.dp))
+                    }
+                }
+                Button(onClick = { onNextClicked(questionIndex.value) },
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .align(alignment = Alignment.CenterHorizontally)
+                        .background(AppColors.mLightBlue),
+                    shape = RoundedCornerShape(34.dp),
+
+                    content = {
+                        Text(text = "Next",
+                            modifier = Modifier.padding(4.dp),
+                            color = AppColors.mOffWhite,
+                            fontSize = 17.sp)
+
+                    })
+
+            }
         }
     }
 }
@@ -73,5 +206,63 @@ fun QuestionTracker(counter: Int = 10,
         }
     },
         modifier = Modifier.padding(20.dp))
-
 }
+
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    Canvas(modifier = Modifier.fillMaxSize().height(1.dp) ) {
+        drawLine(color = AppColors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, 0f),
+            pathEffect = pathEffect)
+    }
+}
+@Preview
+@Composable
+fun ShowProgress(score: Int = 12) {
+
+    val gradient = Brush.linearGradient(listOf(Color(0xFFF95075),
+        Color(0xFFBE6BE5)))
+
+    val progressFactor by remember(score) {
+        mutableStateOf(score*0.005f)
+
+    }
+    Row(modifier = Modifier
+        .padding(3.dp)
+        .fillMaxWidth()
+        .height(45.dp)
+        .border(width = 4.dp,
+            brush = Brush.linearGradient(colors = listOf(AppColors.mLightPurple,
+                AppColors.mLightPurple)),
+            shape = RoundedCornerShape(34.dp))
+        .clip(RoundedCornerShape(topStartPercent = 50,
+            topEndPercent = 50,
+            bottomEndPercent = 50,
+            bottomStartPercent = 50))
+        .background(Color.Transparent),
+        verticalAlignment = Alignment.CenterVertically) {
+        Button(
+            contentPadding = PaddingValues(1.dp),
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth(progressFactor)
+                .background(brush = gradient),
+            enabled = false,
+            elevation = null,
+            ) {
+            Text(text = (score*10).toString(),
+                modifier = Modifier.clip(shape = RoundedCornerShape(23.dp))
+                    .fillMaxHeight(0.87f)
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                color = AppColors.mOffWhite,
+                textAlign = TextAlign.Center)
+
+//
+        }
+
+
+    }
+}
+
